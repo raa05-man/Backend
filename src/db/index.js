28ca73e6affd44a9f4c8a32bda900b29678344b2
Raman Kumar <187097814+raa05-man@users.noreply.github.com>
2025-12-31 17:48:12 +0530

    connection to mongoDB atlas

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose" ;
+import { DB_NAME } from "../constants.js" ;
+
+export const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`MongoDB connected successfully ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB connection failed:", error);
+        process.exit(1) ;
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,36 @@
+// require ('dotenv').config({path: "./env"}) ;
+import dotenv from "dotenv" ;
+import { connectDB } from "./db/index.js" ;
+
+dotenv.config({
+    path: "./env"
+}) ;
+(async () => {
+    await connectDB() ;
+})() ;
+
+
+
+
+/*
+import mongoose from "mongoose" ;
+import { DB_NAME } from "./constants";
+import express from "express" ;
+const app = express() ;
+
+ (async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`) ;
+        app.on("error", (error) => {
+            console.error("ERROR:", error);
+            throw error ;
+        }) ;
+        app.listen(process.env.PORT || 3000, ()=>{
+            console.log(`Server is running on port ${process.env.PORT || 3000}`) ;
+        }) ;
+    }catch(error){
+        console.error("ERROR:", error);
+        throw error ;
+    }
+ })()
+    */
