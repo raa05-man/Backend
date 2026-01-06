c680ce043d9a0b20b89309e14d9f17d7341bb070
Raman Kumar <187097814+raa05-man@users.noreply.github.com>
2026-01-06 15:21:52 +0530

    route for register user

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import {v2 as cloudinary} from "cloudinary" ;
+import { response } from "express";
+import fs from "fs" ;
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try{
+        if(!localFilePath) return null ;
+        // upload file on cloudinary
+        const res = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        // file uploaded successfully
+        console.log("FILE IS UPLOADED ON CLOUDINARY!!",response.url) ;
+        return response ; 
+    }catch(error){
+        fs.unlinkSync(localFilePath) // removes the locally saved temporary file as the upload operation got failed
+        return null ;
+    }
+}
+
+
+// cloudinary.v2.uploader
+// .upload("dog.mp4", {
+//   resource_type: "video", 
+//   public_id: "my_dog",
+//   overwrite: true, 
+//   notification_url: "https://mysite.example.com/notify_endpoint"})
+// .then(result=>console.log(result));
