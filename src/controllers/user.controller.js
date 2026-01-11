1f5d474d2e529ba92df944c9ad58e9ad568f506a
Raman Kumar <187097814+raa05-man@users.noreply.github.com>
2026-01-11 15:00:30 +0530

    login and logged out user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,7 @@ const generateAccessAndRefereshTokens = async(userId) =>{
     }
 }
 
-export const registerUser = asyncHandler( async (req, res) => {
+const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
     // validation - not empty
     // check if user already exists: username, email
@@ -98,3 +98,82 @@ export const registerUser = asyncHandler( async (req, res) => {
     )
 
 } )
+
+const loginUser = asyncHandler(async (req,res)=>{
+    // req body -> data 
+    //username or email 
+    // find the user
+    // password check
+    // access and refresh token
+    // send cookie
+
+    const {username , email , password} = req.body ;
+    if(!username || !email){
+        throw new ApiError(400, "username or email is required")
+    }
+
+    const user = await User.findOne({$or:[{username},{email}]})
+
+    if(!user){
+        throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    
+    if(!isPasswordValid){
+        throw new ApiError(401, "Invalid Password")
+    }
+
+   const {accessToken,refreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+   const loggedInUser = await User.findById(user._id)
+   select("-password , -refreshToken")
+
+   const options = {
+    httpOnly:true,   // now coookies can be modified by server only 
+    secure:true
+   }
+
+   return res
+   .status(200)
+   .cookie("accessToken",accessToken,options)
+   .cookie("refreshToken",refreshToken,options)
+   .json(
+    new ApiResponse(
+        200,    // status code
+        {
+            user:loggedInUser,accessToken,refreshToken   // data
+        },
+        "User logged In Successfully"   // message
+    )
+   )
+})
+
+
+const logoutUser = asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,{
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new:true
+        }
+    ) 
+    const options = {
+    httpOnly:true,   // now coookies can be modified by server only 
+    secure:true
+   }
+   return res
+   .status(200)
+   .clearCookie("accessToken",options)
+   .clearCookie("refreshToken",options)
+   .json(new ApiResponse(200,{},"User logged Out"))
+})
+
+export{
+    registerUser,
+    loginUser,
+    logoutUser
+}
